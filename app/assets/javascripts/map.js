75cce9ee8310219f1ff795db50b8deee27652624
Joshua Baker <joshualbaker111@gmail.com>
2015-08-22 20:20:27 -0700

    map is back

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,28 +1,34 @@
 $(function(){
-
+  // Enables string interpolation on string objects ect..
+  String.prototype.supplant = function (o) {
+    return this.replace(/{([^{}]*)}/g,
+        function (a, b) {
+            var r = o[b];
+            return typeof r === 'string' || typeof r === 'number' ? r : a;
+        }
+      );
+  };
    // render map
   L.mapbox.accessToken = 'pk.eyJ1Ijoiam9zaGxiYWtlcjExIiwiYSI6ImE0ZjEzNjY0OGE5NzM0OWFlZTJiMzhjN2EwMjBjYmNiIn0.-X5q9AKOOsIA-OQPrbOPTw';
-
+                         
   var map = L.mapbox.map('map', 'joshlbaker11.n899ei0m', {
-            center: [37.787, -122.437],
+            center: [37.762, -122.437],
             zoom: 13,
             minZoom: 2,
             zoomControl: false,
             maxBounds: [[-90, -180], [90, 180]]
-
   });
-
       // map.dragging.disable();
       map.touchZoom.disable();
       map.doubleClickZoom.disable();
       map.scrollWheelZoom.disable();
       // Disable tap handler, if present.
       if (map.tap) map.tap.disable();
-
   //set variables
   var geocoder = L.mapbox.geocoder('mapbox.places-v1');
   // function to show markers on map
-  var showMarker = function(lng, lat, name, phone) {
+  var showMarker = function(lng, lat, name, phone, link) {
+    // console.log("link in showMarker: ", link)
     L.mapbox.featureLayer({
       type: 'Feature',
       geometry: {
@@ -30,24 +36,24 @@ $(function(){
         coordinates: [lng, lat]
       },
       properties: {
-      description: "<strong>" link + name + "</strong>" + "<br>" + phone,
+      description: "<a href='" + link + "'>" + name + "</a>" + "<br>" + phone,
+        'url': 'https://www.google.com',
         'marker-size': 'medium',
         'marker-color': '#0967A9',
         'marker-symbol': 'circle-stroked'
       }
     }).addTo(map);
   };
-
   $.get("/pages/busAPI",function(data){
-    console.log(data);
     data.forEach(function(businesses){
       var lat = parseFloat(businesses.lat);
       var lng = parseFloat(businesses.lng);
       var name = businesses.name;
-      var phone = businesses.phone;
-      var link = "a href='businesses/1'"
-      showMarker(lng, lat, name, phone);
+      var id = businesses.id+'';
+      var link = 'http://localhost:3000/businesses/{id}'.supplant({id: id})
+      var phone = businesses.phone;     
+      showMarker(lng, lat, name, phone, link);
     });
-  });
-
+  });  
 });
+Add Comment
