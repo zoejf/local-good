63c7cc2fdb6ff6cd1df78bbeb136e6556e2ffda2
Joshua Baker <joshualbaker111@gmail.com>
2015-08-22 19:16:48 -0700

    made pins link to correlating business show page

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,7 +1,13 @@
 class BusinessesController < ApplicationController
+  
   def index
+  	@business = Business.all
+  	render :index
   end
 
   def show
+  	@bus = Business.find(params[:id])
+  	render :show
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 	
   resources :pages 
 
+  resources :businesses
+
 end
 
 #           Prefix Verb   URI Pattern                 Controller#Action
