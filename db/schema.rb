c01893c9a4b77e5025c5f8fe0c2dd29f28c57e78
GregOSF <gregory.onza@gmail.com>
2015-08-22 17:07:11 -0700

    Migrate schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150822224452) do
+ActiveRecord::Schema.define(version: 20150822225646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 20150822224452) do
     t.integer  "inventory_count"
     t.float    "price"
     t.string   "image"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "phone_number"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
