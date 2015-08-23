030410770bdee4979811ba1451a6b515406f22c9
GregOSF <gregory.onza@gmail.com>
2015-08-22 17:06:28 -0700

    Add routes for users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
+
+  get "/signup", to: "users#new"
+
+  resources :users
+
+  resources :sessions
+
   get 'pages/home'
 
+  root 'site#index'
+
+  
 end
